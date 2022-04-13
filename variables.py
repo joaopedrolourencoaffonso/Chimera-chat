93d049497cdf85c8084c2ef703cc5593d82ef68c
joaopedrolourencoaffonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2022-04-12 22:20:05 -0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+#Exemplo de arquivo variables.py
+api_id = 1234567
+api_hash = "qwerty123qwerty123yqwerty123"
+step_ca_IP = "192.168.0.155:8443"
+registration_IP = "192.168.0.155:8000"
+broadcast_IP = "192.168.0.255"
